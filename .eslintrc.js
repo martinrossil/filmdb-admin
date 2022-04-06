84f64c9b7eac5e99b699ae1f02dc4c0a17522d97
Martin Rossil <martinrossil@gmail.com>
2022-04-06 19:41:08 +0200

    Created lint config.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 11,
+        sourceType: 'module'
+    },
+    ignorePatterns: ['public/', 'development/'],
+    env: {
+        browser: true // makes HTMLElement and customElements NOT no-undef
+    },
+    plugins: [
+        '@typescript-eslint'
+    ],
+    extends: [
+        'plugin:@typescript-eslint/recommended',
+        'standard'
+    ],
+    rules: {
+        indent: 'off',
+        semi: 'off',
+        '@typescript-eslint/no-empty-interface': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        'prefer-spread': 'off',
+        'no-useless-call': 'off',
+        'no-unused-vars': 'off',
+        'space-before-function-paren': 'off',
+        'dot-notation': 'off',
+        'no-new': 'off'
+    }
+}
