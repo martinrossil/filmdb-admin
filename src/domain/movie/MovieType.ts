9fe03be5e01b0374fb0747581e8d7bbeef3ff6c5
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:09:33 +0200

    Created MovieType.

--- src/domain/movie/MovieType.ts
+++ src/domain/movie/MovieType.ts
@@ -1,20 +1,22 @@
-/* eslint-disable camelcase */
+import { CountryType } from '../country/CountryType';
+import { GenreType } from '../genre/GenreType';
+
 export type MovieType = {
-    adult: boolean,
-    backdrop_path: string | null,
-    budget: number,
-    id: number,
-    imdb_id: string | null;
+    adult: boolean;
+    _id: string | null;
+    id: number;
+    title: string;
+    budget: number;
     original_language: string;
     original_title: string;
     overview: string | null;
     popularity: number;
-    poster_path: string | null;
     release_date: string;
     revenue: number;
     runtime: number | null;
     status: string;
-    tagline: string | null;
-    title: string;
     vote_average: number;
+    vote_count: number;
+    genres: Array<GenreType>;
+    production_countries: Array<CountryType>;
 }
