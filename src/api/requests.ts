e2e37c7646e771434285c79084ed5d2de4de9d0c
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:07:46 +0200

    Created reuquests.

--- src/api/requests.ts
+++ src/api/requests.ts
@@ -1,8 +1,26 @@
+import Movie from '../domain/movie/Movie';
 import { headers } from './headers';
-import { getMovieWithHighestIdQuery } from './queries';
+import { createMovieQuery, getMovieWithHighestIdQuery } from './queries';
+
+type RequestInit = {
+    method: string,
+    headers: {
+        'Content-Type': string,
+        authorization: string
+    },
+    body: string
+}
 
 export const getMovieWithHighestIdRequestInit = {
     method: 'POST',
     headers: headers,
     body: getMovieWithHighestIdQuery
+}
+
+export function createMovieRequestInit(movie: Movie): RequestInit {
+    return {
+        method: 'POST',
+        headers: headers,
+        body: createMovieQuery(movie)
+    }
 }
