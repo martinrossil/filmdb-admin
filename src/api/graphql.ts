27f239cbf2e7ef5d7766dbc4a8f0d13080c73bdd
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:07:04 +0200

    Added graphql.

--- src/api/graphql.ts
+++ src/api/graphql.ts
@@ -1,8 +1,26 @@
 import { catchError, getFaunaError } from './errors';
-import { getMovieWithHighestIdRequestInit } from './requests';
-import { GetMovieWithHighestIdResponse } from './responses';
+import { createMovieRequestInit, getMovieWithHighestIdRequestInit } from './requests';
+import { CreateMovieResponse, GetMovieWithHighestIdResponse } from './responses';
 import { fauna } from './urls';
 import Movie from '../domain/movie/Movie';
+
+export async function createMovie(movie: Movie): Promise<[Movie | null, Error | null]> {
+    try {
+        const response: Response = await fetch(fauna, createMovieRequestInit(movie));
+        if (response.ok) {
+            const result = await response.json() as CreateMovieResponse;
+            if (result.data) {
+                return [new Movie(result.data.createMovie), null];
+            }
+            if (result.errors && result.errors.length > 0) {
+                return [null, getFaunaError(result.errors[0])]
+            }
+        }
+        return [null, new Error('Unknown Error')];
+    } catch (error) {
+        return catchError(error);
+    }
+}
 
 export async function getMovieWithHighestId(): Promise<[Movie | null, Error | null]> {
     try {
