dcc4c79d92cf5dd74553922e8180eaa2f74b18cb
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:08:29 +0200

    Created url's.

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -5,9 +5,9 @@ const BASE = 'https://api.themoviedb.org/3/';
 export const fauna = 'https://graphql.eu.fauna.com/graphql';
 
 export function getMovieUrl(id: number): string {
-    return BASE + 'movie/' + id + '?api_key=' + TMDB_KEY + '&include_adult=false&append_to_response=images,alternative_titles,credits,release_dates,translations';
+    return BASE + 'movie/' + id + '?api_key=' + TMDB_KEY + '&include_adult=false&language=da-DK&append_to_response=images,alternative_titles,credits,release_dates,translations&include_image_language=en,da,null';
 }
 
 export function getPersonUrl(id: number): string {
-    return BASE + 'person/' + id + '?api_key=' + TMDB_KEY + '&include_adult=false&append_to_response=images,credits,translations';
+    return BASE + 'person/' + id + '?api_key=' + TMDB_KEY + '&include_adult=false&&language=da-DKappend_to_response=images,credits,translations&include_image_language=en,da,null';
 }
