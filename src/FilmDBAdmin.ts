aca4e7e3e2da3e209ffeb55a9daa4179c3bc8f7f
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:09:51 +0200

    Testing.

--- src/FilmDBAdmin.ts
+++ src/FilmDBAdmin.ts
@@ -1,43 +1,56 @@
 import { Application } from 'fuix';
-import { getMovieWithHighestId } from './api/graphql';
+import { createMovie, getMovieWithHighestId } from './api/graphql';
 import { getMovie, getPerson } from './api/tmdb';
+import { FAUNA_SECRET_FILMDB } from './Config';
 export default class FilmDBAdmin extends Application {
     public constructor() {
         super();
         this.style.height = '100vh';
         this.bodyBackgroundColor = '#000d1a';
-        // this.addEventListener('click', this.start);
-        // this.start();
-        this.loadMovieFromTMDB(49051);
-        // this.loadPersonFromTMDB(1327);
+        this.addEventListener('click', this.start);
     }
 
     private currentIndex = NaN;
 
     private async start(): Promise<void> {
-        const [movie, error] = await getMovieWithHighestId();
+        this.loadMovieFromTMDB(13);
+        /* const [movie, error] = await getMovieWithHighestId();
         if (movie) {
-            this.loadMovieFromTMDB(movie.id + 1);
+            const nextId = movie.id + 1;
+            if (nextId < 25) {
+                this.loadMovieFromTMDB(movie.id + 1);
+            } else {
+                console.log('DONE!!');
+            }
         } else {
             if (error instanceof TypeError) {
                 console.log('Network error.');
             } else {
                 this.loadMovieFromTMDB(0);
             }
-        }
+        } */
     }
 
     private async loadMovieFromTMDB(id: number): Promise<void> {
         console.log('loadMovieFromTMDB(' + id + ')');
         const [movie, error] = await getMovie(id);
         if (movie) {
-            console.log(movie);
-            // create movie in fauna
+            const [created, error] = await createMovie(movie);
+            console.log(created, error);
+            if (error) {
+                console.log(error);
+                // return;
+            }
+            // return;
+            /* const nextId = movie.id + 1;
+            if (nextId < 25) {
+                this.loadMovieFromTMDB(nextId);
+            } else {
+                console.log('DONE!!');
+            } */
         } else {
             console.log(error);
-            setTimeout(() => {
-                this.loadMovieFromTMDB(id + 1);
-            }, 500);
+            // this.loadMovieFromTMDB(id + 1);
         }
     }
 
