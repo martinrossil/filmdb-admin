ac2d45e54620b36a32322b1222eca2803c53ff5e
Martin Rossil <martinrossil@gmail.com>
2022-07-16 16:09:17 +0200

    Created Movie.

--- src/domain/movie/Movie.ts
+++ src/domain/movie/Movie.ts
@@ -1,43 +1,44 @@
 /* eslint-disable camelcase */
-import MovieInterface from './MovieInterface';
+import { CountryType } from '../country/CountryType';
+import { GenreType } from '../genre/GenreType';
 import { MovieType } from './MovieType';
 
-export default class Movie implements MovieInterface {
+export default class Movie {
     public adult: boolean;
-    public backdrop_path: string | null;
-    public budget: number;
+    public title: string;
     public id: number;
-    public imdb_id: string | null;
+    public _id: string | null = null;
+    public budget: number;
     public original_language: string;
     public original_title: string;
-    public overview: string | null;
+    public overview: string | null = null;
     public popularity: number;
-    public poster_path: string | null;
     public release_date: string;
     public revenue: number;
     public runtime: number | null;
     public status: string;
-    public tagline: string | null;
-    public title: string;
     public vote_average: number;
+    public vote_count: number;
+    public genres: Array<GenreType>;
+    public production_countries: Array<CountryType>;
 
-    public constructor(init: MovieType) {
-        this.adult = init['adult'];
-        this.backdrop_path = init['backdrop_path'];
-        this.budget = init['budget'];
-        this.id = init['id'];
-        this.imdb_id = init['imdb_id'];
-        this.original_language = init['original_language'];
-        this.original_title = init['original_title'];
-        this.overview = init['overview'];
-        this.popularity = init['popularity'];
-        this.poster_path = init['poster_path'];
-        this.release_date = init['release_date'];
-        this.revenue = init['revenue'];
-        this.runtime = init['runtime'];
-        this.status = init['status'];
-        this.tagline = init['tagline'];
-        this.title = init['title'];
-        this.vote_average = init['vote_average'];
+    public constructor({ adult, id, title, budget, original_language, original_title, overview, popularity, release_date, revenue, runtime, status, vote_average, vote_count, genres, production_countries, _id }: MovieType) {
+        this.adult = adult;
+        this.title = title;
+        this.id = id;
+        this._id = _id;
+        this.budget = budget;
+        this.original_language = original_language;
+        this.original_title = original_title;
+        this.overview = overview;
+        this.popularity = popularity;
+        this.release_date = release_date;
+        this.revenue = revenue;
+        this.runtime = runtime;
+        this.status = status;
+        this.vote_average = vote_average;
+        this.vote_count = vote_count;
+        this.genres = genres;
+        this.production_countries = production_countries;
     }
 }
