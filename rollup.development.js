e816da671a6a779ee9056791872c1f65a2a803e0
Martin Rossil <martinrossil@gmail.com>
2022-12-29 16:26:52 +0100

    WIP

--- rollup.development.js
+++ rollup.development.js
@@ -3,23 +3,22 @@ import serve from 'rollup-plugin-serve';
 import livereload from 'rollup-plugin-livereload';
 import resolve from '@rollup/plugin-node-resolve';
 
-export default [{
-        input: './src/FilmDBAdmin.ts',
-        plugins: [
-            typescript({ tsconfig: 'tsconfig.development.json' }),
-            resolve(),
-            serve({
-                contentBase: 'development',
-                open: true,
-                historyApiFallback: true,
-                port: 10003
-            }),
-            livereload()
-        ],
-        output: {
-            file: './development/esnext.js',
-            format: 'esm',
-            sourcemap: 'inline'
-        }
+export default {
+    input: './src/FilmDBAdmin.ts',
+    plugins: [
+        typescript({ tsconfig: 'tsconfig.development.json' }),
+        resolve(),
+        serve({
+            contentBase: 'development',
+            open: true,
+            historyApiFallback: true,
+            port: 3000
+        }),
+        livereload()
+    ],
+    output: {
+        file: './development/esnext.js',
+        format: 'esm',
+        sourcemap: 'inline'
     }
-]
+}
