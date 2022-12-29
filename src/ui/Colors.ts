87a71892b0c13f30b28bfc773ef78371b3028b8a
Martin Rossil <martinrossil@gmail.com>
2022-12-29 16:25:35 +0100

    Colors.

---
+++ src/ui/Colors.ts
@@ -0,0 +1,15 @@
+export default class Colors {
+    public static WHITE = 'white';
+    public static BLACK = 'black';
+    // Gray Neutral
+    // public static NEUTRAL_50 = '#F9FAFB';
+    public static NEUTRAL_100 = '#F2F4F7';
+    public static NEUTRAL_200 = '#EAECF0';
+    // public static NEUTRAL_300 = '#D2D6DB';
+    // public static NEUTRAL_500 = '#667085';
+    public static NEUTRAL_600 = '#475467';
+    public static NEUTRAL_900 = '#101828';
+
+    // Blue
+    public static PRIMARY_500 = '#2E90FA';
+}
